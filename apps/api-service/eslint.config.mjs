ffe2fe87163ab08df5a394de8ff18fa939365126
Alex <alexmargaryan18@gmail.com>
2025-05-04 20:21:07 +0400

    initial commit

---
+++ apps/api-service/eslint.config.mjs
@@ -0,0 +1,3 @@
+import { config } from "@amour/eslint-config/service";
+
+export default config;
---
+++ packages/eslint-config/types.d.ts
@@ -0,0 +1,5 @@
+declare module "@amour/eslint-config/service" {
+  import { ESLint } from "eslint";
+  const config: ESLint.ConfigData;
+  export default config;
+}
