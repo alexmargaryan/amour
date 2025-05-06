d05f83afc70419a4ede71abcaa7511be2d1403a3
Alex <alexmargaryan18@gmail.com>
2025-05-06 22:50:15 +0400

    add eslint config in the root of the repo

---
+++ apps/api-service/eslint.config.js
@@ -0,0 +1,10 @@
+import { config } from "@amour/eslint-config/service";
+
+export default [
+  ...config,
+  {
+    rules: {
+      // Add stricter or looser rules for this app
+    },
+  },
+];
--- apps/api-service/eslint.config.mjs
+++
@@ -1,3 +0,0 @@
-import { config } from "@amour/eslint-config/service";
-
-export default config;
