00954a92f58cdfaa174a95b05f2828507c06ae99
Alex <alexmargaryan18@gmail.com>
2025-05-10 20:25:43 +0400

    add global exception filter and swagger to api-service

--- apps/api-service/src/app.module.ts
+++ apps/api-service/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { Logger, Module } from "@nestjs/common";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
@@ -8,6 +8,6 @@ import { PrismaModule } from "./prisma/prisma.module";
 @Module({
   imports: [ApiConfigModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Logger],
 })
 export class AppModule {}
