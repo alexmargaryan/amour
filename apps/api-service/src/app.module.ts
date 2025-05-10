14e37d8787250304fb3cb69cc4089990df5823e2
Alex <alexmargaryan18@gmail.com>
2025-05-10 20:04:17 +0400

    add config module to api-service

--- apps/api-service/src/app.module.ts
+++ apps/api-service/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ApiConfigModule } from "./config/config.module";
 import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [ApiConfigModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/api-service/src/config/swagger-options.ts
@@ -0,0 +1,5 @@
+export const swaggerOptions = {
+  title: "Amour API",
+  description: "Amour Rest Api",
+  version: "0.0.1",
+};
